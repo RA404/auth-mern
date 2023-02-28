9ff6b16f83fa59abf9213b8ada27acde1a0a151e
RA404 <Repin404@gmail.com>
2023-03-01 00:42:07 +0400

    add user and token models

---
+++ server/models/user-model.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    email: {type: String, require: true, unique: true},
+    password: {type: String, require},
+    isActivated: {type: Boolean, default: false},
+    activationLink: {type: String},
+});
+
+module.export = UserSchema('User', UserSchema);
+
