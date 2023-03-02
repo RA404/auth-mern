8ab62724cf1c05cf0b09f15a94856c6196edeb21
RA404 <Repin404@gmail.com>
2023-03-02 23:43:03 +0400

    add services user, mail and tokens

---
+++ server/service/mail-service.js
@@ -0,0 +1,5 @@
+class MailService {
+    sendActivationMail(to, link) {}
+}
+
+module.exports = new MailService();
---
+++ server/service/user-service.js
@@ -0,0 +1,32 @@
+const UserModel = require("../models/user-model");
+const bcrypt = require("bcrypt");
+const uuid = requirei("uuid");
+const mailService = require("./mail-service");
+const tokenService = require("./token-service");
+const UserDto = require("../dto/user-dto");
+
+
+class UserService {
+  async reqistration({ email, password }) {
+    const condidate = await UserModel.findOne({ email });
+    if (condidate) {
+      throw new Error("This user already exist. Please login.");
+    }
+    const activationLink = uuid.v4();
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = await UserModel.create({
+      email,
+      password: hashPassword,
+      activationLink,
+    });
+    await mailService.sendActivationMail(email, activationLink);
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({...userDto});
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {...tokens, user: userDto}
+  }
+}
+
+module.exports = new UserService();
