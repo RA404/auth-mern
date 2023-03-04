08d6832e26faba556388d3464c933a98c11fbcf1
RA404 <Repin404@gmail.com>
2023-03-05 01:45:20 +0400

    add errors middleware

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,61 +1,59 @@
-const userService = require('../service/user-service');
+const userService = require("../service/user-service");
 
-class UserController{
-    
-    async registration(req, res, next) {
-        try {
-           const {email, password} = req.body;
-           const userData = await userService.reqistration(email,password);
-           res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
-           
-           return res.json(userData);
-        } catch (error) {
-            console.log(error);
-        }
+class UserController {
+  async registration(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const userData = await userService.reqistration(email, password);
+      res.cookie("refreshToken", userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+
+      return res.json(userData);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async login(req, res, next) {
-        try {
-            
-        } catch (error) {
-            
-        }
+  async login(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async logout(req, res, next) {
-        try {
-            
-        } catch (error) {
-            
-        }
+  async logout(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async activate(req, res, next) {
-        try {
-            const activationLink = req.params.link;
-            await userService.activate(activationLink);
-            return res.redirect(process.env.CLIENT_URL);
-        } catch (error) {
-            console.log(error);
-        }
+  async activate(req, res, next) {
+    try {
+      const activationLink = req.params.link;
+      await userService.activate(activationLink);
+      return res.redirect(process.env.CLIENT_URL);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async refresh(req, res, next) {
-        try {
-            
-        } catch (error) {
-            
-        }
+  async refresh(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async getUsers(req, res, next) {
-        try {
-            res.json(['123', '234'])
-        } catch (error) {
-            
-        }
+  async getUsers(req, res, next) {
+    try {
+      res.json(["123", "234"]);
+    } catch (error) {
+      next(error);
     }
-
+  }
 }
 
 module.exports = new UserController();
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -4,13 +4,13 @@ const uuid = require("uuid");
 const mailService = require("./mail-service");
 const tokenService = require("./token-service");
 const UserDto = require("../dto/user-dto");
-
+const ApiError = require("../exceptions/api-error");
 
 class UserService {
   async reqistration(email, password) {
     const condidate = await UserModel.findOne({ email });
     if (condidate) {
-      throw new Error("This user already exist. Please login.");
+      throw ApiError.BadRequest("This user already exists. Please login.");
     }
     const activationLink = uuid.v4();
     const hashPassword = await bcrypt.hash(password, 10);
@@ -19,19 +19,22 @@ class UserService {
       password: hashPassword,
       activationLink,
     });
-    await mailService.sendActivationMail(email, `${process.env.API_URL}/api/v1/activate/${activationLink}`);
+    await mailService.sendActivationMail(
+      email,
+      `${process.env.API_URL}/api/v1/activate/${activationLink}`
+    );
 
     const userDto = new UserDto(user);
-    const tokens = tokenService.generateTokens({...userDto});
+    const tokens = tokenService.generateTokens({ ...userDto });
     await tokenService.saveToken(userDto.id, tokens.refreshToken);
 
-    return {...tokens, user: userDto}
+    return { ...tokens, user: userDto };
   }
 
   async activate(activationLink) {
     const user = await UserModel.findOne({ activationLink });
     if (!user) {
-        throw new Error('The activation link is incorrect')
+      throw ApiError.BadRequest("The activation link is incorrect");
     }
     user.isActivated = true;
     await user.save();
