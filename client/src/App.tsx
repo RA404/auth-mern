ae64d1302cb69bb9407f945c7dc4fa780c195479
RA404 <Repin404@gmail.com>
2023-03-06 18:11:58 +0400

    add logout button and functions logaut and refresh token

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,33 @@
-import React, { FC } from "react";
+import React, { FC, useContext, useEffect } from "react";
+import { Context } from "./index";
 import LoginForm from "./components/LoginForm";
+import { observer } from "mobx-react-lite";
 
 const App: FC = () => {
+  const { store } = useContext(Context);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      store.checkAuth();
+    }
+  }, [store]);
+
+  if (store.isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (!store.isAuth) {
+    return (
+      <LoginForm />
+    )
+  }
+
   return (
     <div>
-      <LoginForm />
+      <h1>{store.isAuth ? `Hello ${store.user.email}` : `Please authorize`}</h1>
+      <button onClick={() => store.logout()}>Log Out</button>
     </div>
   );
 };
 
-export default App;
+export default observer(App);
