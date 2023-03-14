88afb4ba3bbfe89b019f4e852aed6c0da81b8e36
RA404 <Repin404@gmail.com>
2023-03-14 18:33:17 +0400

    a bit change register form style

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,30 +1,80 @@
 import React, { FC, useContext, useState } from "react";
 import { Context } from "../index";
 import { observer } from "mobx-react-lite";
+import CSS from "csstype";
 
 const LoginForm: FC = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const { store } = useContext(Context);
 
+  const formContainer: CSS.Properties = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "350px",
+    margin: "100px auto",
+    boxSizing: "border-box",
+    background: "white",
+    borderRadius: "10px",
+    padding: "25px 0",
+  };
+
+  const formHeader: CSS.Properties = {
+    boxSizing: "border-box",
+    width: "350px",
+    margin: "5px",
+    textAlign: "center",
+  };
+
+  const formInput: CSS.Properties = {
+    boxSizing: "border-box",
+    width: "340px",
+    margin: "5px",
+  };
+
+  const formButton: CSS.Properties = {
+    boxSizing: "border-box",
+    margin: "5px",
+  };
+
+  const formOverlay: CSS.Properties = {
+    top: 0,
+    position: "fixed",
+    width: "100%",
+    height: "100%",
+    background: "rgba(0, 0, 0, 0.5)",
+  };
+
   return (
-    <div>
-      <input
-        value={email}
-        type="text"
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        value={password}
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={() => store.login(email, password)}>Log In</button>
-      <button onClick={() => store.registration(email, password)}>
-        Create new account
-      </button>
+    <div style={formOverlay}>
+      <div style={formContainer}>
+        <h2 style={formHeader}>Log-in form</h2>
+        <input
+          style={formInput}
+          value={email}
+          type="text"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          style={formInput}
+          value={password}
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button style={formButton} onClick={() => store.login(email, password)}>
+          Log In
+        </button>
+        <button
+          style={formButton}
+          onClick={() => store.registration(email, password)}
+        >
+          Create new account
+        </button>
+      </div>
     </div>
   );
 };
